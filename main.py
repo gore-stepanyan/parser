af72c4be2219d96ba2aa544ab390fde1d8a8d753
Andrej Mazurov <89501431007and@gmail.com>
2023-04-18 23:05:52 +0700

    удаление лишних обработчиков

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from time import time, sleep
 
 handlers = []
 
-
-
 def sniff():
     capture1 = pyshark.FileCapture(input_file='long.pcap', display_filter='', use_json=True, include_raw=True)
     capture2 = pyshark.FileCapture(input_file='short.pcap', display_filter='', use_json=True, include_raw=True)
@@ -17,7 +15,6 @@ def sniff():
             yield packet
 
 def main():
-    #packetHandler = PacketHandler()
     busy_count = 0
     packet = Packet()
     handlers.append(PacketHandler())
@@ -37,8 +34,16 @@ def main():
             if handler.state == State.HANDLING_SIP_200_OK or handler.state == State.HANDLING_RTP_FLOW:
                 busy_count = busy_count + 1
         
-        if len(handlers) <= busy_count:
+        if len(handlers) == busy_count:
             handlers.append(PacketHandler())
+        
+        if len(handlers) - busy_count >= 2:
+            for handler in handlers:
+                if handler.state == State.HANDLING_SIP_INVITE:
+                    handlers.remove(handler)
+                    break
+
+        
 
 
 
